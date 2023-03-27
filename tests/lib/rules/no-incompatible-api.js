b02bdb37d46c78bbd7988cbfb818a962868332ba
xupea <sysu.peter.hsu@gmail.com>
2023-03-27 11:17:32 +0800

    feat: ready for release

--- tests/lib/rules/no-incompatible-api.js
+++ tests/lib/rules/no-incompatible-api.js
@@ -19,111 +19,111 @@ const ruleTester = new RuleTester();
 ruleTester.run("no-incompatible-api", rule, {
   valid: [
     // https://www.electronjs.org/docs/latest/api/extensions#chromedevtoolsinspectedwindow
-    "chrome.devtools.inspectedWindow.a",
+    "chrome.devtools.inspectedWindow.allPropertiesMethodsEvents",
     // https://www.electronjs.org/docs/latest/api/extensions#chromedevtoolsnetwork
-    // "devtools.network",
-    // // https://www.electronjs.org/docs/latest/api/extensions#chromedevtoolspanels
-    // "devtools.panels",
-    // // // https://www.electronjs.org/docs/latest/api/extensions#chromeextension
-    // "extension.lastError",
-    // "extension.getURL()",
-    // "extension.getBackgroundPage()",
-    // // // https://www.electronjs.org/docs/latest/api/extensions#chromeruntime
+    "devtools.network.allPropertiesMethodsEvents",
+    // https://www.electronjs.org/docs/latest/api/extensions#chromedevtoolspanels
+    "devtools.panels.allPropertiesMethodsEvents",
+    // // https://www.electronjs.org/docs/latest/api/extensions#chromeextension
+    "extension.lastError",
+    "extension.getURL()",
+    "extension.getBackgroundPage()",
+    // // https://www.electronjs.org/docs/latest/api/extensions#chromeruntime
     "chrome.runtime.lastError",
-    // "runtime.id",
-    // "runtime.getBackgroundPage()",
-    // "runtime.getManifest()",
-    // "runtime.getPlatformInfo()",
-    // "runtime.getURL()",
-    // "runtime.connect()",
-    // "runtime.sendMessage()",
-    // "runtime.reload()",
-    // "runtime.onStartup",
-    // "runtime.onInstalled",
-    // "runtime.onSuspend",
-    // "runtime.onSuspendCanceled",
-    // "runtime.onConnect",
-    // "runtime.onMessage",
-    // // // https://www.electronjs.org/docs/latest/api/extensions#chromewebrequest
-    // "storage.local",
-    // // // https://www.electronjs.org/docs/latest/api/extensions#chrometabs
-    // "tabs.sendMessage",
-    // "tabs.reload",
-    // "tabs.executeScript",
-    // "tabs.update",
-    // // // https://www.electronjs.org/docs/latest/api/extensions#chromemanagement
-    // "management.getAll",
-    // "management.get",
-    // "management.getSelf",
-    // "management.getPermissionWarningsById",
-    // "management.getPermissionWarningsByManifest",
-    // "management.onEnabled",
-    // "management.onDisabled",
-    // // // https://www.electronjs.org/docs/latest/api/extensions#chromewebrequest
-    // "chrome.webRequest",
+    "runtime.id",
+    "runtime.getBackgroundPage()",
+    "runtime.getManifest()",
+    "runtime.getPlatformInfo()",
+    "runtime.getURL()",
+    "runtime.connect()",
+    "runtime.sendMessage()",
+    "runtime.reload()",
+    "runtime.onStartup",
+    "runtime.onInstalled",
+    "runtime.onSuspend",
+    "runtime.onSuspendCanceled",
+    "runtime.onConnect",
+    "runtime.onMessage",
+    // // https://www.electronjs.org/docs/latest/api/extensions#chromewebrequest
+    "storage.local",
+    // // https://www.electronjs.org/docs/latest/api/extensions#chrometabs
+    "tabs.sendMessage",
+    "tabs.reload",
+    "tabs.executeScript",
+    "tabs.update",
+    // // https://www.electronjs.org/docs/latest/api/extensions#chromemanagement
+    "management.getAll",
+    "management.get",
+    "management.getSelf",
+    "management.getPermissionWarningsById",
+    "management.getPermissionWarningsByManifest",
+    "management.onEnabled",
+    "management.onDisabled",
+    // // https://www.electronjs.org/docs/latest/api/extensions#chromewebrequest
+    "chrome.webRequest.allPropertiesMethodsEvents",
   ],
   
   invalid: [
-    // // extension
-    // {
-    //   code: "extension.inIncognitoContext",
-    //   errors: [
-    //     {
-    //       message: "extension.inIncognitoContext is supported by electron.",
-    //     },
-    //   ],
-    // },
-    // {
-    //   code: "extension.getExtensionTabs()",
-    //   errors: [
-    //     {
-    //       message: "extension.getExtensionTabs is supported by electron.",
-    //     },
-    //   ],
-    // },
-    // // runtime 
-    // {
-    //   code: "runtime.openOptionsPage",
-    //   errors: [
-    //     {
-    //       message: "runtime.openOptionsPage is supported by electron.",
-    //     },
-    //   ],
-    // },
-    // // storage
-    // {
-    //   code: "storage.sync",
-    //   errors: [
-    //     {
-    //       message: "storage.sync is supported by electron.",
-    //     },
-    //   ],
-    // },
-    // {
-    //   code: "storage.managed",
-    //   errors: [
-    //     {
-    //       message: "storage.managed is supported by electron.",
-    //     },
-    //   ],
-    // },
-    // // tabs
-    // {
-    //   code: "tabs.captureVisibleTab",
-    //   errors: [
-    //     {
-    //       message: "tabs.captureVisibleTab is supported by electron.",
-    //     },
-    //   ],
-    // },
-    // // management
-    // {
-    //   code: "management.otherMethods",
-    //   errors: [
-    //     {
-    //       message: "management.otherMethods is supported by electron.",
-    //     },
-    //   ],
-    // },
+    // extension
+    {
+      code: "chrome.extension.inIncognitoContext",
+      errors: [
+        {
+          message: "chrome.extension.inIncognitoContext is supported by electron.",
+        },
+      ],
+    },
+    {
+      code: "chrome.extension.getExtensionTabs()",
+      errors: [
+        {
+          message: "chrome.extension.getExtensionTabs is supported by electron.",
+        },
+      ],
+    },
+    // runtime 
+    {
+      code: "chrome.runtime.openOptionsPage",
+      errors: [
+        {
+          message: "chrome.runtime.openOptionsPage is supported by electron.",
+        },
+      ],
+    },
+    // storage
+    {
+      code: "chrome.storage.sync",
+      errors: [
+        {
+          message: "chrome.storage.sync is supported by electron.",
+        },
+      ],
+    },
+    {
+      code: "chrome.storage.managed",
+      errors: [
+        {
+          message: "chrome.storage.managed is supported by electron.",
+        },
+      ],
+    },
+    // tabs
+    {
+      code: "chrome.tabs.captureVisibleTab",
+      errors: [
+        {
+          message: "chrome.tabs.captureVisibleTab is supported by electron.",
+        },
+      ],
+    },
+    // management
+    {
+      code: "chrome.management.otherMethods",
+      errors: [
+        {
+          message: "chrome.management.otherMethods is supported by electron.",
+        },
+      ],
+    },
   ],
 });
